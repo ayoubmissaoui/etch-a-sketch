b2faacb82382b965ecde77a5eaf38f6f196e4243
Ayoub Missaoui <missaouimedayoub@gmail.com>
2022-01-18 13:19:58 +0100

    Edit boxes border settings

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// create 16x16 grid
-const gridContainer = document.querySelector('#grid');
+// 16x16 grid that you can play with 
 
-function makeCells (){
-    for (let i = 0 ; i < 256 ; i ++) {
-        const newDiv = document.createElement('div');
-        gridContainer.appendChild(newDiv);
-        console.log("added");
-    }
+const divContainer = document.querySelector("#grid");
+
+function makeDefaultGrid() {
+    for ( let i = 0 ; i < 256 ; i ++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("box");
+        divContainer.appendChild(newDiv);
+        }
 }
 
-makeCells()
-
+makeDefaultGrid();

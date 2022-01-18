88e76f1289e7fd6220c7f594ce392334c467db76
Ayoub Missaoui <missaouimedayoub@gmail.com>
2022-01-18 12:01:42 +0100

    Write script to create 16X16 grid of divs, and add styling for divs

---
+++ script.js
@@ -0,0 +1,12 @@
+// create 16x16 grid
+const gridContainer = document.querySelector('#grid');
+
+function makeCells (){
+    for (let i = 0 ; i < 256 ; i ++) {
+        const newDiv = document.createElement('div');
+        gridContainer.appendChild(newDiv);
+        console.log("added");
+    }
+}
+makeCells()
+
